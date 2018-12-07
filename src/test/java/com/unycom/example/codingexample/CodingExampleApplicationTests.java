2eb77df263f9572638e05ad94c27bd670ea48d55
Max Sinelnikov <sinelnikov@xprom.ru>
2018-12-07 19:16:20 +0300

    Added some it tests

--- src/test/java/com/unycom/example/codingexample/CodingExampleApplicationTests.java
+++ src/test/java/com/unycom/example/codingexample/CodingExampleApplicationTests.java
@@ -1,16 +1,73 @@
 package com.unycom.example.codingexample;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CodingExampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
+
+	@LocalServerPort
+	private int port;
+
+	private final String host = "http://localhost";
+
+	TestRestTemplate restTemplate = new TestRestTemplate();
+	private String getUrl(String path) {
+		return String.format("%s:%d/%s", host, port, path);
 	}
 
+	@Test
+	public void testGetListOfCustomers() {
+		ResponseEntity<String> response = restTemplate.getForEntity(getUrl("/api/v1/customers"), String.class);
+		assertEquals(200, response.getStatusCodeValue());
+		assertTrue(response.getBody().contains("Haydn"));
+		assertTrue(response.getBody().contains("Mozart"));
+		assertTrue(response.getBody().contains("Beethoven"));
+	}
+
+	@Test
+	public void testGetCustomer() {
+		ResponseEntity<String> response = restTemplate.getForEntity(getUrl("/api/v1/customers/0001"), String.class);
+		assertEquals(200, response.getStatusCodeValue());
+		assertTrue(response.getBody().contains("Haydn"));
+	}
+
+	@Test
+	public void testGetUnknownCustomer() {
+		ResponseEntity<?> response = restTemplate.getForEntity(getUrl("/api/v1/customers/XXX-UNKNOWN"), String.class);
+		assertEquals(404, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void testGetListOfOrders() {
+		ResponseEntity<String> response = restTemplate.getForEntity(getUrl("/api/v1/orders"), String.class);
+		assertEquals(200, response.getStatusCodeValue());
+		assertTrue(response.getBody().contains("violin")); // the store without violins can't pass this test
+	}
+
+	@Test
+	public void testGetOrder() {
+		ResponseEntity<String> response = restTemplate.getForEntity(getUrl("/api/v1/orders/1"), String.class);
+		assertEquals(200, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void testGetUnknownOrder() {
+		ResponseEntity<?> response = restTemplate.getForEntity(getUrl("/api/v1/orders/1000001"), String.class);
+		assertEquals(404, response.getStatusCodeValue());
+	}
 }
